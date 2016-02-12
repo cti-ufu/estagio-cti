b7b9729e3d7caeca08fce5608ad15cf3b253a22a
letmagnino <leticiamagninof@gmail.com>
2016-02-12 16:35:53 -0200

    Criação de slides

--- codigo/estagio/src/test/java/br/ufu/cti/estagio/testes/ExemploDeTeste.java
+++ codigo/estagio/src/test/java/br/ufu/cti/estagio/testes/ExemploDeTeste.java
@@ -2,19 +2,20 @@ package br.ufu.cti.estagio.testes;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ExemploDeTeste {
 
 	@Test
 	public void testandoAssertEqualSuccess() {
-
+		System.out.println("Leticia");
 		// Teste com sucesso
 		assertEquals("String", "String");
 
 	}
 
-	@Test
+	@Ignore @Test
 	public void testandoAssertEqualFail() {
 
 		// Teste com falha
