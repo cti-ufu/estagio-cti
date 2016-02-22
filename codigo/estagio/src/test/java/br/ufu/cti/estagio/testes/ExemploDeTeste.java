3726ef1a00b12cb6d7c93459bb18905062dbbd0a
letmagnino <leticiamagninof@gmail.com>
2016-02-22 13:27:54 -0300

    Criação de slides capitulo 7,8 e 9

--- codigo/estagio/src/test/java/br/ufu/cti/estagio/testes/ExemploDeTeste.java
+++ codigo/estagio/src/test/java/br/ufu/cti/estagio/testes/ExemploDeTeste.java
@@ -2,7 +2,13 @@ package br.ufu.cti.estagio.testes;
 
 import static org.junit.Assert.*;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
+public class ExemploDeTeste {
 
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
